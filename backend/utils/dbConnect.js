c8847beb54ed99cce05f45c141ea6a3636845173
daryna-hatsosh <daryna.hatsosh@gmail.com>
2025-01-11 18:16:35 +0200

    test mongo connection

--- backend/utils/dbConnect.js
+++ backend/utils/dbConnect.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+  throw new Error('Please define the MONGODB_URI environment variable.');
 }
 
 let cached = global.mongoose;
@@ -20,6 +20,7 @@ async function dbConnect() {
 
   if (!cached.promise) {
     console.log('Creating new connection');
+    console.log(MONGODB_URI);
     cached.promise = mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
